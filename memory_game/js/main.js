41494b8220f01e7df33f3b35a75a2af570e8b2a9
Molly Stone <molly.stone89@gmail.com>
2019-02-26 10:25:52 -0500

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,19 @@
-cardOne = "queen";
-cardTwo = "queen";
-cardThree = "king";
-cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardTwo);
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+console.log('User flipped ' + cardOne + '.');
+console.log('User flipped ' + cardTwo + '.');
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	} else {
+		alert('Sorry, try again.');
+	}
+}
